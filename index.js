9531167514aa2b974dbdbcf690f0152c5c544391
BilmerIslamov <bilmerlibrary@gmail.com>
2024-03-09 10:58:30 +0500

    2-dars

---
+++ index.js
@@ -0,0 +1,39 @@
+var yosh = +prompt("Yoshingizni kiriting")
+if(yosh>=100){
+    alert("Umringiz uzoq bolsin")
+}else if(yosh>=90){
+    alert("100 yowlaga kirib yuring")
+}else if(yosh>=80){
+    alert(" Sog' salomat bo'lin")
+}else if(yosh>=70){
+    alert("Nabiralar davrasida dam oling")
+}else if(yosh>=60){
+    alert("Yaqinda pensaner bolasiz")
+}else if (yosh>=50){
+    alert("Bolaringizni baxtiga sog' bolin")
+}else if(yosh>=30){
+    alert("Qaysi iwda iwlamoqda siz")
+}else if(yosh>=20){
+    alert("Armiyaga borgan misiz ")
+}else if(yosh>=18){
+        alert("Insitituga kira oldingizmi")
+}else if(yosh>=16){
+    alert("Maktabda qoldingizmi")
+}else if(yosh>=7){
+    alert("Makatabga chiqdingizmi")
+}else if(yosh>=3){
+    alert("Bog' chaga borgan misiz")
+}else if(yosh>=0){
+    alert("Siz hali yoshga tolmagansiz")
+}else if (yosh){
+    alert("nimadir xato kedi")
+}
+var a = 15
+var b = 10
+var c = 5
+var son = +prompt("1dan 20magacha son kiritib omadingizni sinab ko'ring")
+if(son==a || son==b || son==c){
+    alert("Tabriklaymiz siz 100ming so'm yutib oldingiz")
+}else{
+    alert("Afsuzki bu sfar omadingiz kelmadi")
+}
